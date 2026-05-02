6f95a7ae885bebac624eabbe09a9be2cd04e4dd7
aicifares <faresaici4@gmail.com>
2026-05-02 11:57:11 +0000

    corrections navigation et dashboard-admin

--- auth-service/auth_service/urls.py
+++ auth-service/auth_service/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-from django.conf import settings
 import os
 
-FRONTEND_DIR = os.path.join(settings.BASE_DIR, 'frontend')
+FRONTEND_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'frontend')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
-    
-    # Servir les fichiers frontend
     path('auth/login.html', serve, {'path': 'login.html', 'document_root': FRONTEND_DIR}),
     path('auth/register.html', serve, {'path': 'register.html', 'document_root': FRONTEND_DIR}),
     path('auth/dashboard.html', serve, {'path': 'dashboard.html', 'document_root': FRONTEND_DIR}),
